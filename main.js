0e484c6b253739d5e635e29382274b6b68d7cda2
bmarcko <bobbymarckocruz@gmail.com>
2022-10-07 16:18:36 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let first_num = 123,
+    second_num = 456;
+
+function sum(){
+    alert(`The sum of ${first_num} and ${second_num} is ${first_num+second_num}`);
+}
+
+function product(){
+    alert(`The product of ${first_num} and ${second_num} is ${first_num*second_num}`);
+}
